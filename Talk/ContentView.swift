f2482d38ae2b0435dca62706502d6e6b96ffbd42
jiamingkong <kinetical@live.com>
2026-03-20 00:01:55 +0800

    Initial Commit

---
+++ Talk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Talk
+//
+//  Created by 孔嘉明 on 2026/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Talk/TalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkApp.swift
+//  Talk
+//
+//  Created by 孔嘉明 on 2026/3/20.
+//
+
+import SwiftUI
+
+@main
+struct TalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
